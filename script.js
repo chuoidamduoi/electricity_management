fbcddd53efa8828f5959ab7009a3ffddc18a1b81
chuoidamduoi <ntqnhu1324@gmail.com>
2025-03-10 19:32:03 +0700

    edit api

--- script.js
+++ script.js
@@ -103,8 +103,8 @@ function getPayment() {
 }
 // --------------------- Các hàm lưu trữ dữ liệu qua Google Sheets ---------------------
 // Chú ý: Thay đổi SCRIPT_URL thành URL Web App đã triển khai từ Render
-// const SCRIPT_URL = "https://electricity-management-server.onrender.com/api";
-const SCRIPT_URL = " http://localhost:3000/api"
+const SCRIPT_URL = "https://electricity-management-server.onrender.com/api";
+// const SCRIPT_URL = " http://localhost:3000/api"
 // Lấy danh sách HOUSESHOLDS từ Google Sheets
 async function getHouseholds(type) {
     isLoading(true)
